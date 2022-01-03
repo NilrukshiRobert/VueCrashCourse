a2cdb54c44380238101f7fec675174151add3278
Nilrukshi Robert <66418638+NilrukshiRobert@users.noreply.github.com>
2022-01-03 14:30:56 +0800

    Vue learning

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,62 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-</template>
+<div class="container">
+  <Header title="Task Tracker"/>
+  <Tasks :tasks="tasks" />
+  </div>
+  </template>
 
-<script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import HelloWorld from './components/HelloWorld.vue';
+<script>
+import Header from './components/Header.vue'
+import Tasks from './components/Tasks.vue'
 
-@Options({
-  components: {
-    HelloWorld,
-  },
-})
-export default class App extends Vue {}
+export default {
+   name:'App',
+   components:{
+     Header,
+     Tasks
+   },
+   data(){
+     return{
+       tasks:[]
+     }
+   },
+   created(){
+     this.tasks= [
+     {
+       id:1,
+       text:'Doctors Appointment',
+       day:'March 1st at 2.30pm',
+       reminder:true,
+     },
+     {
+       id:2,
+       text:'Meeting at School',
+       day:'March 3rd at 1.30pm',
+       reminder:true,
+     },
+     {
+       id:3,
+       text:'Food Shoping',
+       day:'March 14th at 1.30pm',
+       reminder:false,
+     }
+     ]
+   }
+ }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .btn{
+    display: inline-block;
+    background: darkblue;
+    color:white;
+    border: none;
+    padding: 10px 20px;
+    margin:5px;
+    border-radius: 5px;
+    cursor: pointer;
+    text-decoration: none;
+    font-size: 15px;
+    font-family: inherit;
+  }
 </style>
